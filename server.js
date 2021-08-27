c77e9b4201b6420f02b091537dd1d9dbc5da3e67
John Lyons <jwlyons2015@gmail.com>
2021-08-27 00:54:52 -0500

    Express SimpleREST

--- server.js
+++ server.js
@@ -7,6 +7,59 @@ const port = 3000;
 
 const app = express();
 app.use(morgan('dev'));
+//when app recieves json data, it parses json data into javascript properties of data
+app.use(express.json());
+
+//app.all is a routing method that is a catch all for http verbs
+app.all('/campsites', (req, res, next) => {
+    res.statusCode = 200;
+    //setHeader -- sending plain text in response body
+    res.setHeader('Content-Type', 'text/plain');
+    //next passes control of application routing to relevant routing. Without it, the data would just stop here and not go anywhere
+    next();
+});
+
+//setting up endpoint for get request for campsites
+//tales callback function
+app.get('/campsites', (req, res) => {
+    //response status and header code already set up with app.all on line 14
+    res.end('Will send all the campsites to you');
+});
+
+//takes information to next relevant information, after going through app.all
+app.post('/campsites', (req, res) => {
+    res.end(`Will add the campsite: ${req.body.name} with description: ${req.body.description}`);
+});
+
+app.put('/campsites', (req, res) => {
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /campsites');
+});
+
+//deletes campsites
+app.delete('/campsites', (req, res) => {
+    res.end('Deleting all campsites');
+});
+
+//allows us to store whatever client sends as part of the path after slash as route parameter named campsiteId
+app.get('/campsites/:campsiteId', (req, res) => {
+    res.end(`Will send details of campsite: ${req.params.campsiteID} to you`);
+});
+
+app.post('/campsites/:campsiteId', (req, res) => {
+    res.statusCode = 403;
+    res.end(`POST operation not supported on /campsites/${req.params.campsiteId}`);
+});
+
+app.put('/campsites/:campsiteId', (req, res) => {
+    res.write(`Updating the campsite: ${req.params.campsiteId}\n`);
+    res.end(`Will update the campsite ${req.body.name}
+    with description: ${req.body.description}`);
+});
+
+app.delete('/campsites/:campsiteId', (req, res) => {
+    res.end(`Deleting campsite: ${req.params.campsiteId}`);
+});
 
 //dirname refers to absolute path it is in
 app.use(express.static(__dirname + '/public'));
