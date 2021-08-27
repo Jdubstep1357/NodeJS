79130b5707530dfda35dee27e7c8a20d90903ca1
John Lyons <jwlyons2015@gmail.com>
2021-08-26 23:45:22 -0500

    Intro to Express Part 2

--- server.js
+++ server.js
@@ -1,21 +1,25 @@
 const express = require('express');
+//middleware for logging server requests
+const morgan = require('morgan');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
+app.use(morgan('dev'));
 
-//app.use((STUFF IN HERE)) is callback function
-//req is request object
-//res is response object
-//next is function
+//dirname refers to absolute path it is in
+app.use(express.static(__dirname + '/public'));
+
+
 app.use((req, res) => {
-    console.log(req.headers);
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
     res.end('<html><body><h1>This is an Express Server</h1></body></html>');
 });
 
+//creates instance of server class and listens to it
 app.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
+
